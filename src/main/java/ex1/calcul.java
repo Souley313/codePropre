18d142d130061b7321e45da88fb047f06bc15ec3
Souleymane THIAM <souleymanethiam22@gmail.com>
2020-06-23 21:02:24 +0200

    03.1- TP-Nettoyage.Ex1

--- src/main/java/ex1/calcul.java
+++ src/main/java/ex1/calcul.java
@@ -1,28 +1,31 @@
 package ex1;
 
 /**
- * Classe qui réalise des opérations arythmétiques
+ * Classe qui Réalise des opération arythmétiques
+ * 
  * @author DIGINAMIC
  */
 public class calcul {
 
-	/** Méthode qui retourne l'addition des paramètres a et b 
-	 * 
-	 * @param a paramètre a
-	 * @param b paramètre b
-	 * @return int 
-	 */
-	public int addition(int a, int b){
-		return a+b;
-	}
-	
 	/**
+	 * Méthode qui retourne l'addition des param�tres a et b
 	 * 
-	 * @param a
-	 * @param b
-	 * @return
+	 * @param a un entier
+	 * @param b un entier
+	 * @return a+b
 	 */
-	public int get2(int a, int b){
-		return a-b;
+	public int addition(int a, int b) {
+		return a + b;
+	}
+
+	/**
+	 * M�thode qui retourne la soustraction des param�tres a et b
+	 * 
+	 * @param a un entier
+	 * @param b un entier
+	 * @return a-b
+	 */
+	public int get2(int a, int b) {
+		return a - b;
 	}
 }
--- src/main/java/ex1/entreprise.java
+++ src/main/java/ex1/entreprise.java
@@ -2,17 +2,31 @@ package ex1;
 
 import java.util.Date;
 
+/**
+ * Classe qui représente une entreprise
+ * 
+ * @author DIGINAMIC
+ */
 public class entreprise {
 
+	/** Siret de l'entreprise */
 	public int Siret;
+	/** Nom de l'entreprise */
 	public String Nom;
+	/** Adrese de l'entreprise */
 	public String adresse;
+	/** Date de création */
 	public Date date_Creation;
-	
+
+	/** CAPITAL_MAX capital maximum */
 	public static final int capitalMax = 3000000;
-	
-	public void Afficher_statut(){
-		
+
+	/**
+	 * 
+	 * 
+	 */
+	public void Afficher_statut() {
+
 	}
-	
+
 }
